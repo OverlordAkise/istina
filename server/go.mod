5631dd7f70ee7c15e91d0d489f50fe5799cfa598
OverlordAkise <OverlordAkise@gmail.com>
2023-06-30 00:13:35 +0200

    [server] Added prometheus metrics

--- server/go.mod
+++ server/go.mod
@@ -6,27 +6,34 @@ require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/prometheus/client_golang v1.16.0
 	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.10.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ server/metrics.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"strconv"
+	"time"
+    "net/http"
+)
+
+func RegisterMetrics(r *gin.Engine) {
+    
+    //Counter for only uphill data, Vec for variable labels
+	requestCounter := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "istina",
+			Name:      "http_requests_total",
+			Help:      "Number of received http requests",
+		},
+		[]string{"code", "method", "host", "url"},
+	)
+	prometheus.MustRegister(requestCounter)
+    
+    // Histogram for buckets, Vec for variable labels
+	durationCounter := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: "istina",
+			Name:      "http_duration_seconds",
+			Help:      "HTTP request latencies in seconds",
+            Buckets: []float64{0.0001,0.001,0.01,0.1,0.3,0.5,1,2},
+		},
+		[]string{"code", "method", "url"},
+	)
+	prometheus.MustRegister(durationCounter)
+    
+    //Summary for variable data that maybe doesnt fit into buckets
+	sizeCounter := prometheus.NewSummary(
+		prometheus.SummaryOpts{
+			Namespace: "istina",
+			Name:      "http_request_size_bytes",
+			Help:      "HTTP request size in bytes",
+		},
+	)
+	prometheus.MustRegister(sizeCounter)
+
+	//Middleware
+	r.Use(func(c *gin.Context) {
+		t := time.Now()
+		//bodylength, _ := io.Copy(ioutil.Discard, c.Request.Body)
+        bodylength := computeApproximateRequestSize(c.Request)
+
+		c.Next()
+
+		elapsed := float64(time.Since(t)) / float64(time.Second)
+		status := strconv.Itoa(c.Writer.Status())
+
+		if c.Request.URL.Path != "/metrics" && c.Request.URL.Path != "/favicon.ico" {
+			requestCounter.WithLabelValues(status, c.Request.Method, c.Request.Host, c.Request.URL.Path).Inc()
+			durationCounter.WithLabelValues(status, c.Request.Method, c.Request.URL.Path).Observe(elapsed)
+			sizeCounter.Observe(float64(bodylength))
+		}
+	})
+
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+}
+
+// From https://github.com/zsais/go-gin-prometheus/blob/2199a42d96c1d40f249909ed2f27d42449c7fc94/middleware.go#L397
+func computeApproximateRequestSize(r *http.Request) int {
+	s := 0
+	if r.URL != nil {
+		s = len(r.URL.Path)
+	}
+
+	s += len(r.Method)
+	s += len(r.Proto)
+	for name, values := range r.Header {
+		s += len(name)
+		for _, value := range values {
+			s += len(value)
+		}
+	}
+	s += len(r.Host)
+
+	// N.B. r.Form and r.MultipartForm are assumed to be included in r.URL.
+
+	if r.ContentLength != -1 {
+		s += int(r.ContentLength)
+	}
+	return s
+}
